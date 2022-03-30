4cb2b3696ee3040d7bf5194fc2079013a3c14f65
fairwell-coding <coding@temmel.me>
2022-03-30 17:45:01 +0200

    adjusted meshgrid

--- src/meshgrid.py
+++ src/meshgrid.py
@@ -13,18 +13,13 @@ if __name__ == '__main__':
     XX_, YY_ = np.meshgrid(x, y)
     ZZ_ = XX_ + YY_
 
-    # Create 2d grid without using linspace
-    XX_2, YY_2 = np.ogrid[1:4:1, 10:40:10]
-    ZZ_2 = XX_2 + YY_2
-    ZZ_2 = ZZ_2.T
-
     # Create 2d grid using ogrid (does not require linspace)
     XX_2, YY_2 = np.ogrid[1:4:1, 10:40:10]
     ZZ_2 = XX_2 + YY_2
     ZZ_2 = ZZ_2.T
 
     # Create 2d grid using mgrid (does not require linspace)
-    XX_3, YY_3 = np.ogrid[1:4:1, 10:40:10]
+    XX_3, YY_3 = np.mgrid[1:4:1, 10:40:10]
     ZZ_3 = XX_3 + YY_3
     ZZ_3 = ZZ_3.T
 
